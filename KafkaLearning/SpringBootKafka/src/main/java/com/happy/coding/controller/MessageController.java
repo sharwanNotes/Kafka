4fc0def975dd7864d18851aa384cf6d4896f166b
Sharwan Kumar <sharwank@stg.com>
2024-06-17 15:40:04 +0530

    my changes

---
+++ KafkaLearning/SpringBootKafka/src/main/java/com/happy/coding/controller/MessageController.java
@@ -0,0 +1,28 @@
+package com.happy.coding.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.happy.coding.kafka.KafkaProducer;
+
+@RestController
+@RequestMapping("/api/v1/kafka")
+public class MessageController {
+	
+	private KafkaProducer kafkaProducer;
+
+	public MessageController(KafkaProducer kafkaProducer) {
+		this.kafkaProducer = kafkaProducer;
+	}
+	
+	//  http:localhost:8999/api/v1/kafka/publish?message=hello world
+	@GetMapping("/publish")
+	public ResponseEntity<String> publish(@RequestParam("message") String message){
+		kafkaProducer.sendMessage(message);
+		return ResponseEntity.ok("message sent");
+		
+	}
+}
